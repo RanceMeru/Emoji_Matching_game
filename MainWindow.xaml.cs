a92abfc4e05f9b02e68190b9dc8ef1204b4ee233
JLOLS <JLOLS@DESKTOP-67T9OVD>
2022-03-15 15:36:33 -0700

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Headfirst_part_1
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+           List<string> animalEmoji = new List<string>()
+            {
+                "🐙","🐙",
+                "🐡","🐡",
+                "🐘","🐘",
+                "🐳","🐳",
+                "🐪","🐪",
+                "🦕","🦕",
+                "🦘","🦘",
+                "🦔","🦔",
+            };
+
+            Random random = new Random();
+            {
+                foreach (TextBlock textblock in mainGrid.Children.OfType<TextBlock>())
+                {
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                  textblock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
+            }
+        }
+    }
+}
